8b8ef19ede50b0571fca739001033584e79d680a
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-09-24 18:43:15 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118058/latihan13relational/PBOIF210118058Latihan13Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118058.latihan13relational;
+
+/**
+ *
+ * @author user
+ * * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210118058Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1=10;
+        int bil2=20;
+        System.out.println("bil1 == bil2 = "+(bil1==bil2));
+        System.out.println("bil1 != bil2 = "+(bil1!=bil2));
+        System.out.println("bil1 > bil2 = "+(bil1>bil2));
+        System.out.println("bil1 < bil2 = "+(bil1<bil2));
+        System.out.println("bil1 >= bil2 = "+(bil1>=bil2));
+        System.out.println("bil1 <= bil2 = "+(bil1<=bil2));
+    }
+    
+}
